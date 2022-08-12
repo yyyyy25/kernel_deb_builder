9a4fe6afe10a8e7c2f4412b010c577ab45a21298
V1me <z1991998920@gmail.com>
2022-08-12 20:40:36 +0800

    build 2

--- patch.d/remove_trust_keys.sh
+++ patch.d/remove_trust_keys.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sed -i "/CONFIG_SYSTEM_TRUSTED_KEYS/s/^/# /g" .config
-scripts/config --disable SYSTEM_TRUSTED_KEYS 
+sed -i "/CONFIG_SYSTEM_TRUSTED_KEYS/s/\".*\"/\"\"/g" .config
+# scripts/config --disable SYSTEM_TRUSTED_KEYS 
