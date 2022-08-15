8111d5670e7cd1eda5fe0e05db76d59470a47850
V1me <z1991998920@gmail.com>
2022-08-15 13:12:16 +0800

    build 3

---
+++ patch.d/disable_debug_info_btf.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# CONFIG_DEBUG_INFO_BTF=y
+# CONFIG_DEBUG_INFO_BTF_MODULES=y
+
+VERSION_HEADER=$(echo "${VERSION}" | sed -En 's/(.*).[0-9]+/\1/p')
+if [[ "${VERSION_HEADER}" == "5.12" || "${VERSION_HEADER}" == "5.13" ]]; then
+    sed -i "/CONFIG_DEBUG_INFO_BTF/s/y/n/" .config
+fi
