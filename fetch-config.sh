6aecaf06ac69327072847b25a3bf9124bbfbdbe7
V1me <z1991998920@gmail.com>
2022-09-08 17:27:51 +0800

    new

--- fetch-config.sh
+++ fetch-config.sh
@@ -7,25 +7,30 @@ else
     VERSION_MAJOR="${VERSION}"
 fi
 
-mkdir -p .fetch_config
-cd .fetch_config || exit
-
-# get config file from ubuntu ppa
-DEB_FILE=$(curl https://kernel.ubuntu.com/\~kernel-ppa/mainline/v${VERSION_MAJOR}/amd64/ | grep -P -o 'linux-headers-.*?-generic.*?_amd64.deb' | awk 'NR==1{print $1}')
-echo "Fetching ${DEB_FILE} from https://kernel.ubuntu.com/~kernel-ppa/mainline/v${VERSION_MAJOR}/amd64/${DEB_FILE}"
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v${VERSION_MAJOR}/amd64/${DEB_FILE}
-ar x ${DEB_FILE}
-if [ -f "data.tar.xz" ]; then
-    tar xf data.tar.xz
-elif [ -f "data.tar.zst" ]; then
-    cat data.tar.zst | zstd -d | tar xf -
+if [ -f config-${VERSION} ]; then
+    cp config-${VERSION} .config
 else
-    echo "No data.tar.xz or data.tar.zst found"
-    exit 1
+    mkdir -p .fetch_config
+    cd .fetch_config || exit
+
+    # get config file from ubuntu ppa
+    DEB_FILE=$(curl https://kernel.ubuntu.com/\~kernel-ppa/mainline/v${VERSION_MAJOR}/amd64/ | grep -P -o 'linux-headers-.*?-generic.*?_amd64.deb' | awk 'NR==1{print $1}')
+    echo "Fetching ${DEB_FILE} from https://kernel.ubuntu.com/~kernel-ppa/mainline/v${VERSION_MAJOR}/amd64/${DEB_FILE}"
+    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v${VERSION_MAJOR}/amd64/${DEB_FILE}
+    ar x ${DEB_FILE}
+    if [ -f "data.tar.xz" ]; then
+        tar xf data.tar.xz
+    elif [ -f "data.tar.zst" ]; then
+        cat data.tar.zst | zstd -d | tar xf -
+    else
+        echo "No data.tar.xz or data.tar.zst found"
+        exit 1
+    fi
+
+    cd ..
+    cp .fetch_config/usr/src/linux-headers-*/.config ./.config
 fi
 
-cd ..
-cp .fetch_config/usr/src/linux-headers-*/.config ./.config
 GCC_VERSION=$(cat .config | sed -n 's/CONFIG_CC_VERSION_TEXT=".*\s\(.*\)"$/\1/p')
 GCC_VERSION_MAJOR=$(echo "${GCC_VERSION}" | sed -En 's/(.*).[0-9]+/\1/p')
 
