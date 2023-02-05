c0e63ded75820077a5dcca154bd232e6a51596be
V1me <z1991998920@gmail.com>
2023-02-06 06:09:17 +0800

    debug user key(add update)

--- patch.d/debug_user_key.sh
+++ patch.d/debug_user_key.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-sed -i -e '/void user_revoke(struct key \*key)/{n;a\
-\tprintk("V1me: revoke key ========\n");
+sed -i -e '/void user_revoke(struct key \*key)/{n;n;a\
+\tprintk("V1me: revoke key -- %#lx ========\\n", upayload);
 }' security/keys/user_defined.c 
 
 sed -i -e '/static void user_free_payload_rcu(struct rcu_head \*head)/{n;a\
-\tprintk("V1me: free payload callback ========\n");
+\tprintk("V1me: free payload callback ========\\n");
+}' security/keys/user_defined.c
+
+sed -i -e '/int user_update(struct key \*key, struct key_preparsed_payload \*prep)/{n;a\
+\tprintk("V1me: update key -- %#lx ========\\n", prep->payload.data[0]);
 }' security/keys/user_defined.c
