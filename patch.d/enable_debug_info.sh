bc3c2f5fa0b20f24c63c7f480893bdc5a1e9d2cb
V1me <z1991998920@gmail.com>
2023-04-11 19:41:02 +0800

    enable overlayfs

--- patch.d/enable_debug_info.sh
+++ patch.d/enable_debug_info.sh
@@ -2,3 +2,6 @@
 
 # CONFIG_DEBUG_INFO is not set
 sed -i "/CONFIG_DEBUG_INFO/s/# CONFIG_DEBUG_INFO is not set/CONFIG_DEBUG_INFO=y/g" .config
+
+# CONFIG_DEBUG_INFO_NONE=y
+sed -i "/CONFIG_DEBUG_INFO_NONE/s/y/n/g" .config
