70fee7362d2b99ceaf605528ad3ce4df17cf2888
V1me <z1991998920@gmail.com>
2023-04-12 16:27:19 +0800

    enable debug

--- patch.d/enable_debug_info.sh
+++ patch.d/enable_debug_info.sh
@@ -3,3 +3,7 @@
 sed -i "/CONFIG_DEBUG_INFO_NONE/s/y/n/g" .config
 # CONFIG_DEBUG_INFO is not set
 sed -i "/CONFIG_DEBUG_INFO/s/# CONFIG_DEBUG_INFO is not set/CONFIG_DEBUG_INFO=y/g" .config
+
+grep "CONFIG_DEBUG_INFO=y" .config || echo "CONFIG_DEBUG_INFO=y" >> .config 
+grep "CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT=y" .config || echo "CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT=y" >> .config
+grep "CONFIG_DEBUG_INFO_COMPRESSED_NONE=y" .config || echo "CONFIG_DEBUG_INFO_COMPRESSED_NONE=y" >> .config
